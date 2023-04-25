6b1a1d3bcd20cbebc952e8f1aef878a07286c661
Leandro Brangi <leandro.brangi@gmail.com>
2023-04-25 16:59:50 -0300

    DEPLOY DATABASE2

--- api/index.js
+++ api/index.js
@@ -27,8 +27,8 @@ const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001')
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`)
     countriesDb(); // eslint-disable-line no-console
   });
 });
